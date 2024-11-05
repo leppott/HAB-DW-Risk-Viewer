a45193bea23de121e6cbdead0efc2c66060bf6fe
Erik Leppo <Erik.Leppo@tetratech.com>
2024-11-05 16:14:23 -0500

    v0.1.0.9004
    - refactor: Finish setting up files as a proper repo, Issue \#1
    - refactor: Set up dummy Shiny app (Old Faithful)
    - refactor: Modify Shiny to use USEPA template, Issue \#2
      - Separate files for header, footer, css, tags
      - Use files from ContDataQC Shiny app

--- inst/apps/HAB-DW-Risk-Viewer/ui.R
+++ inst/apps/HAB-DW-Risk-Viewer/ui.R
@@ -7,27 +7,42 @@
 #    https://shiny.posit.co/
 #
 
-library(shiny)
+# library(shiny)
 
 # Define UI for application that draws a histogram
 fluidPage(
+  
+  # CSS
+  theme = "styles.css",
+  
+  # Tags
+  shiny::includeCSS(file.path("www", "tags_ContDataQC.R")),
+  
+  # EPA Header
+  # https://www.epa.gov/themes/epa_theme/pattern-lab/patterns/pages-standalone-template/pages-standalone-template.rendered.html
+  # shiny::includeHTML(file.path("www", "header_TADA.html")),
+  shiny::includeHTML(file.path("www", "header_ContDataQC.html")),
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+      sidebarPanel(
+          sliderInput("bins",
+                      "Number of bins:",
+                      min = 1,
+                      max = 50,
+                      value = 30)
+      ),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
+      # Show a plot of the generated distribution
+      mainPanel(
+          plotOutput("distPlot")
+      )
+  ),
+  
+  # EPA footer
+  # shiny::includeHTML(file.path("www", "footer_TADA.html"))
+  shiny::includeHTML(file.path("www", "footer_ContDataQC.html"))
 )
